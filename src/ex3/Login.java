4dc2a37ed70c24cd355fe67f899650297c243e36
Yakov <jsalavize@gmail.com>
2019-05-20 14:01:54 +0300

    Init

---
+++ src/ex3/Login.java
@@ -0,0 +1,20 @@
+package ex3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Login", urlPatterns = "/login")
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("login");
+    }
+}
